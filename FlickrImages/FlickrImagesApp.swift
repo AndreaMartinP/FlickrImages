b9be94a124213884e931641ae7474617720179b9
Andrea Martin <>
2024-01-29 16:37:13 -0600

    initial commit - created project

---
+++ FlickrImages/FlickrImagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrImagesApp.swift
+//  FlickrImages
+//
+//  Created by Andrea Martin  on 1/29/24.
+//
+
+import SwiftUI
+
+@main
+struct FlickrImagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
